38a289c7b0dc7891bae0bb90a930b69d27e747b4
Albert Suarez <albert.suarez.molgo@est.fib.upc.edu>
2015-12-03 08:14:05 +0100

    asdf

---
+++ app/src/main/java/edu/upc/fib/molgo/suarez/albert/remindit/Event.java
@@ -0,0 +1,62 @@
+package edu.upc.fib.molgo.suarez.albert.remindit;
+
+/**
+ * Event class
+ * @author albert.suarez.molgo
+ */
+public abstract class Event {
+
+    /**
+     * Enumeration that describes the type of Event.
+     * @author albert.suarez.molgo
+     */
+    public enum EventType {
+        EVENT_MEETING,
+        EVENT_TASK
+    }
+
+    /**
+     * An event has two possible types.
+     */
+    protected EventType eventType;
+
+    /**
+     * Every event is associated to a calendar.
+     */
+    protected Calendar calendar;
+
+
+
+
+    /**
+     * Get the calendar of the implicit event.
+     * @return The calendar.
+     */
+    public Calendar getCalendar() {
+        return calendar;
+    }
+
+    /**
+     * Set implicit calendar for the calendar's param.
+     * @param calendar The calendar to set.
+     */
+    public void setCalendar(Calendar calendar) {
+        this.calendar = calendar;
+    }
+
+    /**
+     * Indicates if the implicit event is a Meeting or not.
+     * @return If it's true, the implicit event is a Meeting. Otherwise it's a Task.
+     */
+    public boolean isMeeting() {
+        if (eventType == EventType.EVENT_MEETING) return true;
+        return false;
+    }
+
+    /**
+     * Get the String representation of an Event.
+     * @return The String representation.
+     */
+    @Override
+    public abstract String toString();
+}
---
+++ app/src/main/java/edu/upc/fib/molgo/suarez/albert/remindit/MainActivity.java
@@ -0,0 +1,57 @@
+package edu.upc.fib.molgo.suarez.albert.remindit;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.*;
+import android.widget.*;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                test(v);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void test(View view) {
+        switch (view.getId()) {
+            case R.id.button :
+                Button button = (Button) findViewById(R.id.button);
+                TextView text = (TextView) findViewById(R.id.textView);
+
+                if (button.isClickable()) {
+                    text.setText("Hello");
+                }
+                break;
+        }
+    }
+}
