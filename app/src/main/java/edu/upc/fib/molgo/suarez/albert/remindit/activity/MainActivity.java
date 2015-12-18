9d724b6da7703937318e0e0ffceb189cf3449397
Albert Suarez <albert.suarez.molgo@est.fib.upc.edu>
2015-12-18 17:48:36 +0100

    Add CalendarView

--- app/src/main/java/edu/upc/fib/molgo/suarez/albert/remindit/activity/MainActivity.java
+++ app/src/main/java/edu/upc/fib/molgo/suarez/albert/remindit/activity/MainActivity.java
@@ -2,58 +2,26 @@ package edu.upc.fib.molgo.suarez.albert.remindit.activity;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.*;
 import android.widget.*;
 
 import edu.upc.fib.molgo.suarez.albert.remindit.R;
 
 public class MainActivity extends ActionBarActivity {
 
+    CalendarView calendar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                test(v);
+        calendar = (CalendarView) findViewById(R.id.calendar);
+        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+                Toast.makeText(getApplicationContext(), dayOfMonth + "/" + (month+1) + "/" + year, Toast.LENGTH_LONG).show();
             }
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    public void test(View view) {
-        switch (view.getId()) {
-            case R.id.button :
-                Button button = (Button) findViewById(R.id.button);
-                TextView text = (TextView) findViewById(R.id.textView);
-
-                if (button.isClickable()) {
-                    text.setText("Hello");
-                }
-                break;
-        }
-    }
 }
