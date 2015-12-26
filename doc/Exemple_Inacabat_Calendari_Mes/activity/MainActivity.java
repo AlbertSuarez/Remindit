35a1676d4e1325a6d0f8c00a6a419054cc2dfb1d
Elena_Ruiz <elena.ruiz.bdn@gmail.com>
2015-12-26 01:07:09 +0100

    calendar week view finished

--- doc/Exemple_Inacabat_Calendari_Mes/activity/MainActivity.java
+++ doc/Exemple_Inacabat_Calendari_Mes/activity/MainActivity.java
@@ -2,10 +2,13 @@ package edu.upc.fib.molgo.suarez.albert.remindit.activity;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.widget.*;
 
 import edu.upc.fib.molgo.suarez.albert.remindit.R;
 
 public class MainActivity extends ActionBarActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
